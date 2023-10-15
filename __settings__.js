0972af84e824e63d84484aa07228bf558d42ce30
LeeIan0303343 <147879177+LeeIan0303343@users.noreply.github.com>
2023-10-15 10:51:57 -0400

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1879634.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 153665854, 153665890, 153665853, 153665856, 153665849, 153665850, 153665852, 153665883, 153665867, 153665862, 153665868, 153665864, 153665891, 153665833, 153665889 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
